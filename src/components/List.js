8bb4ae84527bef1113dd4b2bfaf379798c653e03
Marcin Gromek <gromson101@gmail.com>
2023-10-11 11:06:52 +0200

    refactor: reuse useLoadPhotos both for initial and scroll photos loading

--- src/components/List.js
+++ src/components/List.js
@@ -1,17 +1,12 @@
 import React, {useEffect} from "react";
-import axios from "axios";
-import {useDispatch, useSelector} from "react-redux";
-import {setPhotos} from "../reducers/app";
+import {useSelector} from "react-redux";
 import {Flex} from "@chakra-ui/react";
-import {useLoadMorePhotos} from "../hooks/useLoadMorePhotos";
-import {API_ACCESS_KEY} from "../shared";
+import {useLoadPhotos} from "../hooks/useLoadPhotos";
 
 const List = () => {
-  const dispatch = useDispatch();
   const photosList = useSelector(state => state.app.list)
   const isLoading = useSelector(state => state.app.isLoading)
-  const page = useSelector(state => state.app.pageCurrent)
-  const loadMorePhotos = useLoadMorePhotos();
+  const loadMorePhotos = useLoadPhotos();
 
   const handleScroll = () => {
     const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
@@ -29,23 +24,7 @@ const List = () => {
   }, [handleScroll]);
 
   useEffect(() => {
-    const getImages = async()=> {
-      try {
-        const response = await axios(`https://api.unsplash.com/photos?per_page=30&page=${page}`, {
-          method: 'GET',
-          headers: {
-            'Authorization': `Client-ID ${API_ACCESS_KEY}`,
-          },
-        });
-        console.log(response.data)
-        dispatch(setPhotos(response.data))
-        console.log(photosList)
-
-      } catch(error) {
-        console.error('Error fetching images from Unsplash:', error);
-      }
-    }
-    getImages()
+    loadMorePhotos()
   }, []);
 
   return <Flex justify="space-around" grow="1" wrap="wrap" className="pt-10 gap-4">
--- src/hooks/useLoadPhotos.js
+++ src/hooks/useLoadPhotos.js
@@ -2,15 +2,15 @@ import {incrementPage, setIsLoading, setPhotos} from "../reducers/app";
 import {useDispatch, useSelector} from "react-redux";
 import {API_ACCESS_KEY} from "../shared";
 
-export const useLoadMorePhotos = () => {
+export const useLoadPhotos = () => {
   const dispatch = useDispatch();
-  const page = useSelector(state => state.app.pageCurrent)
+  const pageCurrent = useSelector(state => state.app.pageCurrent);
 
   return async () => {
     try {
       dispatch(setIsLoading(true));
       const response = await fetch(
-        `https://api.unsplash.com/photos?per_page=30&=page=${page}&client_id=${API_ACCESS_KEY}`,
+        `https://api.unsplash.com/photos?per_page=30&=page=${pageCurrent}&client_id=${API_ACCESS_KEY}`,
         { method: 'GET' }
       );
       const newPhotos = await response.json();
@@ -22,5 +22,4 @@ export const useLoadMorePhotos = () => {
       dispatch(incrementPage());
     }
   }
-
 };
